c08ed9509a5fe3a81768e72120fc4cbda0dcadec
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-02-23 06:15:38 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,30 @@
+
+#include <vector>
+using namespace std;
+
+class Solution {
+    
+public:
+
+    int majorityElement(vector<int>& nums) {
+        int elementWithFrequencyGreaterThanHalf = 0;
+        int balanceOfOccurence = 0;
+        for (const auto& n : nums) {
+            if (balanceOfOccurence == 0) {
+                elementWithFrequencyGreaterThanHalf = n;
+            }
+            balanceOfOccurence += (elementWithFrequencyGreaterThanHalf == n) ? 1 : -1;
+        }
+
+        /*
+        The Boyer-Moore Majority Voting Algorithm requires a second iteration
+        for checking whether the frequency of the found element is
+        actually greater than half.
+
+        However, for this particular case, it is not necessary because
+        the problem statement guarantees that there is such element.
+         */
+
+        return elementWithFrequencyGreaterThanHalf;
+    }
+};
